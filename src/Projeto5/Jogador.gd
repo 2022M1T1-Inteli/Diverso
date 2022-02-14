5ce0a6ba086d444a702d0a1dde7ab790ec64b83a
VZeferino <vitor.queiroz@sou.inteli.edu.br>
2022-02-14 11:21:39 -0300

    Bonequinho movendo

---
+++ src/Projeto5/Jogador.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+const SPEED = 200
+var motion = Vector2()
+
+func _physics_process(delta):
+
+	if Input.is_action_pressed("ui_right"):
+		motion.x = SPEED
+		$AnimatedSprite.play("Run")
+		$AnimatedSprite.flip_h = false
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -SPEED
+		$AnimatedSprite.play("Run")
+		$AnimatedSprite.flip_h = true
+	elif Input.is_action_pressed("ui_up"):
+		motion.y = -SPEED
+		$AnimatedSprite.play("Run")
+	elif Input.is_action_pressed("ui_down"):
+		motion.y = SPEED
+		$AnimatedSprite.play("Run")
+	else:
+		motion.x = 0
+		motion.y = 0
+		$AnimatedSprite.play("Idle")
+	Input
+
+	motion = move_and_slide(motion)
