737006648740602e7f54be48cc77421ded0dd9cb
VZeferino <vitor.queiroz@sou.inteli.edu.br>
2022-02-25 11:50:59 -0300

    Glória sprites

--- src/Projeto5/Jogador.gd
+++ src/Projeto5/Jogador.gd
@@ -15,10 +15,10 @@ func _physics_process(delta):
 		$AnimatedSprite.flip_h = true
 	elif Input.is_action_pressed("ui_up"):
 		motion.y = -SPEED
-		$AnimatedSprite.play("Run")
+		$AnimatedSprite.play("Up")
 	elif Input.is_action_pressed("ui_down"):
 		motion.y = SPEED
-		$AnimatedSprite.play("Run")
+		$AnimatedSprite.play("Down")
 	else:
 		motion.x = 0
 		motion.y = 0
