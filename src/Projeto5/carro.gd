797ab4963f32b11a551555cb183994cb1e5ca908
VZeferino <vitor.queiroz@sou.inteli.edu.br>
2022-02-26 19:08:58 -0300

    Carro

---
+++ src/Projeto5/carro.gd
@@ -0,0 +1,25 @@
+extends StaticBody2D
+
+var flip=true
+var posicao_inicial
+var posicao_final
+
+func _ready():
+	posicao_inicial = $".".position.y-400
+	posicao_final = posicao_inicial+1000
+		
+
+func _process(delta):
+	if(posicao_inicial <= posicao_final and flip):
+		$".".position.y += 2
+		position.x=-130
+		$Sprite.flip_v = false
+		if ($".".position.y >= posicao_final):
+			flip = false
+
+	if($".".position.y >= posicao_inicial and !flip):
+		$".".position.y -= 2
+		position.x=-200
+		$Sprite.flip_v = true
+		if ($".".position.y <= posicao_inicial):
+			flip = true
