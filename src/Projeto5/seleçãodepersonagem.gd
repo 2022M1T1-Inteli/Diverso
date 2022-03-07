51e2619ca02d71d03959c91697db86eecf4520b6
dmsophia <sophia.dias@sou.inteli.edu.br>
2022-03-07 18:27:10 -0300

    início script selecionar personagem

---
+++ src/Projeto5/seleçãodepersonagem.gd
@@ -0,0 +1,8 @@
+extends Control
+
+#o cursor se moverá numa área restrita da seleção de personagem e guardar o personagem selecionado pelo usuário
+
+var characters = []  #guarda a texturerect selecionada
+var currentSelected = 0
+var currentColumnSpot = 0
+var currentRowSpot = 0
