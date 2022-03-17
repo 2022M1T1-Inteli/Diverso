6ffb262dc010ed2ead90f06d47e689a5d0408e24
dmsophia <sophia.dias@sou.inteli.edu.br>
2022-03-17 15:19:25 -0300

    diálogos

---
+++ src/Projeto5/NPC5.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+var active = false
+
+func _ready():
+	connect("body_entered", self, '_on_NPC_body_entered')
+	connect("body_exited", self, '_on_NPC_body_exited')
+
+func _on_NPC_body_entered(body):
+	if body.name == "Jogador":
+		active = true
+
+func _process(delta):
+	$Point.visible = active
+
+func _input(event):
+	if get_node_or_null('DialogNode') == null:
+		if event.is_action_pressed("ui_accept") and active:
+			get_tree().paused = true
+			var dialog = Dialogic.start('timeline5')
+			dialog.pause_mode = Node.PAUSE_MODE_PROCESS
+			dialog.connect('timeline_end', self, 'unpause')
+			add_child(dialog)
+			
+
+func unpause(timeline_timeline1):
+	get_tree().paused = false
+	get_tree().change_scene("res://Scene/Quizz1.tscn")
+
+
+func _on_NPC_body_exited(body):
+	if body.name == "Jogador":
+		active = false
