36dd97129e14fb15617db4cabf5aef97b079c724
VZeferino <vitor.queiroz@sou.inteli.edu.br>
2022-03-23 10:32:51 -0300

    Correção código

--- src/Projeto5/Jogador.gd
+++ src/Projeto5/Jogador.gd
@@ -15,24 +15,31 @@ func _physics_process(delta):
 		motion = motion.normalized()
 		
 		
-		if motion != Vector2.ZERO:
-			velocity = motion * SPEED
-		else:
-			velocity = Vector2.ZERO
-
 		if Input.is_action_pressed("ui_right"):
 			$AnimatedSprite.play("Run")
 			$AnimatedSprite.flip_h = false
 		elif Input.is_action_pressed("ui_left"):
 			$AnimatedSprite.play("Run")
 			$AnimatedSprite.flip_h = true
-			
 		elif Input.is_action_pressed("ui_up"):
 			$AnimatedSprite.play("Up")
 		elif Input.is_action_pressed("ui_down"):
 			$AnimatedSprite.play("Down")
 		else:
 			$AnimatedSprite.play("Idle")
+
+		if motion != Vector2.ZERO:
+			velocity = motion * SPEED
+		else:
+			velocity = Vector2.ZERO
+
+		var actualPosition = get_position() #Pegar a posição do personagem e salvar
+		Globalpos.Position = Vector2(actualPosition) #Utilizar a variável global para salvar pegar a posição em outros scripts
+		move_and_slide(velocity)
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	Globalpos.a = true
+
  #movimentação do personagem principal pelos eixos x e y 
  #o personagem pode se movimentar em diagonal
 #	if Input.is_action_pressed("ui_right"): #movimento do personagem para direita
@@ -53,13 +60,3 @@ func _physics_process(delta):
 #		motion.x = 0
 #		motion.y = 0
 #		$AnimatedSprite.play("Idle") 
-	var actualPosition = get_position() #Pegar a posição do personagem e salvar
-	Globalpos.Position = Vector2(actualPosition) #Utilizar a variável global para salvar pegar a posição em outros scripts
-	move_and_slide(velocity)
-	
-
-
-
-func _on_AnimationPlayer_animation_finished(anim_name):
-	Globalpos.a = true
-	pass # Replace with function body.
