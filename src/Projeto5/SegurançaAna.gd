9cf620367dd5f874b7fa8633e5a3d6cbdf0f161f
dmsophia <sophia.dias@sou.inteli.edu.br>
2022-04-01 00:39:37 -0300

    dialogos e cenario

--- src/Projeto5/SegurançaAna.gd
+++ src/Projeto5/SegurançaAna.gd
@@ -17,16 +17,11 @@ func _input(event):
 	if get_node_or_null('DialogNode') == null:
 		if event.is_action_pressed("ui_accept") and active:
 			get_tree().paused = true
-			var dialog = Dialogic.start('Ana6')
+			var dialog = Dialogic.start('Ana1')
 			dialog.pause_mode = Node.PAUSE_MODE_PROCESS
 			dialog.connect('timeline_end', self, 'unpause')
 			add_child(dialog)
 			
-
-func unpause(timeline_timeline1):
-	get_tree().paused = false
-	Transition.fade_into("res://Scene/QuizzAna5.tscn")
-
 func _on_NPC_body_exited(body):
 	if body.name == "Jogador":
 		active = false
