d26f739a8caac9853b330e3034810f648bfb19b2
gabrielyin <70323043+gabrielyin@users.noreply.github.com>
2022-04-11 11:56:26 -0300

    feedback 3 lucas

--- src/Projeto5/Scene/Lucas/FeedbackLucas/feedbacklucas3b.gd
+++ src/Projeto5/Scene/Lucas/FeedbackLucas/feedbacklucas3b.gd
@@ -17,4 +17,4 @@ func _ready() -> void:
 
 
 func _on_voltar_pressed() -> void:
-	Transition.fade_into("res://Scene/Lucas/QuizLucas/QuizLucas5.tscn") # Replace with function body.
+	Transition.fade_into("res://Scene/Lucas/QuizLucas/QuizLucas3.tscn") # Replace with function body.
